d04b26c625b1d7eca31c082d241d6db112da5ad7
Susumu Yamazaki <zacky1972@gmail.com>
2025-07-13 01:55:51 +0900

    initial commit

---
+++ lib/crandall_reduction.ex
@@ -0,0 +1,18 @@
+defmodule CrandallReduction do
+  @moduledoc """
+  Documentation for `CrandallReduction`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CrandallReduction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crandall_reduction_test.exs
@@ -0,0 +1,8 @@
+defmodule CrandallReductionTest do
+  use ExUnit.Case
+  doctest CrandallReduction
+
+  test "greets the world" do
+    assert CrandallReduction.hello() == :world
+  end
+end
